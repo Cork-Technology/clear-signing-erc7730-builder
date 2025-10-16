5c6b57fc9788fc4e3d756456f227bdb7b3cf315e
Pierre Aoun <pierre.aoun@ledger.fr>
2025-10-16 18:26:19 +0200

    Truncate labels in previews to match real devices behaviour

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function truncateLabel(name: string, maxLength: number = 30): string {
+  if (name.length <= maxLength) {
+    return name;
+  }
+  return name.slice(0, maxLength - 3) + '...';
+}
